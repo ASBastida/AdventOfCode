ad34d901d28c408c218665b2c540d9ac7ea129ad
Alejandro Sánchez Bastida <alejandrosanchez@socialicity.co.uk>
2021-12-04 11:21:59 +0100

    Day 1

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -2,14 +2,55 @@
 
 public class Day_01 : BaseDay
 {
-    private readonly string _input;
+    private readonly IEnumerable<string> _input;
 
     public Day_01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        var totalMax = 0;
+        var lastMax = int.MaxValue;
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        foreach(var value in _input.Select(x => int.Parse(x)))
+        {
+            if(value > lastMax)
+            {
+                totalMax++;
+            }
+            
+            lastMax = value;            
+        }
+
+        return ValueTask.FromResult(totalMax.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var totalMax = 0;
+        var lastMax = int.MaxValue;
+
+        var inputs = _input.Select(x => int.Parse(x)).ToList();
+        var i1 = inputs[0];
+        var i2 = inputs[1];
+
+        foreach(var i in inputs.Skip(2))
+        {
+            var sum = i + i1+i2;
+
+            if(sum > lastMax)
+            {
+                totalMax++;
+            }
+
+            lastMax = sum;
+
+            i1=i2;
+            i2=i;
+        }        
+
+        return ValueTask.FromResult(totalMax.ToString());
+    }
 }
